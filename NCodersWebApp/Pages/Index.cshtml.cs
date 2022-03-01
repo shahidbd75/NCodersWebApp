e046aeb43340ad06729988833304dc8b99344b6e
Khandakar Rowshan Kibria <rowshan.kibria@iqvia.com>
2022-03-01 13:28:24 +0600

    Add project files.

---
+++ NCodersWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NCodersWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private UserManager<IdentityUser> _userManager;
+
+        public IndexModel(ILogger<IndexModel> logger, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
